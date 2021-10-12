0ad0821b195257844e81a15ecd1e8df0ac3881dc
Myles Gordon <myles.gordon@sky.uk>
2021-10-12 16:24:59 +0100

    Royal family task cleanup

--- familyTree/royalFamily.js
+++ familyTree/royalFamily.js
@@ -18,33 +18,28 @@ const generation1 = [
   new Person("Queen Elizabeth", []),
 ];
 
+const george_liz = [
+  generation1.find((person) => person.name === "King George VI"),
+  generation1.find((person) => person.name === "Queen Elizabeth"),
+];
+
 const generation2 = [
   new Person("Prince Philip", []),
-  new Person("Queen Elizabeth II", [
-    generation1.find((person) => person.name === "King George VI"),
-    generation1.find((person) => person.name === "Queen Elizabeth"),
-  ]),
-  new Person("Princess Margaret", [
-    generation1.find((person) => person.name === "King George VI"),
-    generation1.find((person) => person.name === "Queen Elizabeth"),
-  ]),
+  new Person("Queen Elizabeth II", george_liz),
+  new Person("Princess Margaret", george_liz),
+];
+
+const phil_liz2 = [
+  generation2.find((person) => person.name === "Prince Philip"),
+  generation2.find((person) => person.name === "Queen Elizabeth II"),
 ];
 
 const generation3 = [
   new Person("Camila", []),
-  new Person("Charles", [
-    generation2.find((person) => person.name === "Prince Philip"),
-    generation2.find((person) => person.name === "Queen Elizabeth II"),
-  ]),
+  new Person("Charles", phil_liz2),
   new Person("Diana", []),
-  new Person("Anne", [
-    generation2.find((person) => person.name === "Prince Philip"),
-    generation2.find((person) => person.name === "Queen Elizabeth II"),
-  ]),
-  new Person("Prince Edward", [
-    generation2.find((person) => person.name === "Prince Philip"),
-    generation2.find((person) => person.name === "Queen Elizabeth II"),
-  ]),
+  new Person("Anne", phil_liz2),
+  new Person("Prince Edward", phil_liz2),
 ];
 
 console.log(generation3[3].childOf());
