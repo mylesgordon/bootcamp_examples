c4bfa379a4546accd114333e23d0aacf09a1ff04
Myles Gordon <myles.gordon@sky.uk>
2021-10-13 14:29:35 +0100

    Royal family test now uses toBe rather than ===

--- familyTree/royalFamily.test.js
+++ familyTree/royalFamily.test.js
@@ -4,7 +4,7 @@ let generations = royalFamilyModule.findGenerations();
 
 describe("Check that we receive an array of Generation classes from findGeneration", () => {
   test("Checking for correct array length", () => {
-    expect(generations.length === 3);
+    expect(generations.length).toBe(3);
   });
 
   test("Checking that each of the array members are of type Generation", () => {
