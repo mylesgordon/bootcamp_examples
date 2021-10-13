aa9a3d13560252139d3918d7ade4cd33bcc2b3de
Myles Gordon <myles.gordon@sky.uk>
2021-10-13 14:46:54 +0100

    Added parent test

--- familyTree/royalFamily.test.js
+++ familyTree/royalFamily.test.js
@@ -12,4 +12,32 @@ describe("Check that we receive an array of Generation classes from findGenerati
       expect(generation).toBeInstanceOf(royalFamilyModule.Generation)
     );
   });
+
+  test("Checking that everyone who has defined parents has them initiated", () => {
+    let noParents = [
+      "King George VI",
+      "Queen Elizabeth",
+      "Prince Philip",
+      "Camila",
+      "Diana",
+    ];
+
+    for (let x = 0; x < generations.length; x++) {
+      let currentGenerationMembers = generations[x].members;
+      for (let y = 0; y < currentGenerationMembers.length; y++) {
+        // if it isn't a royal family member who has no defined parents
+        if (noParents.includes(currentGenerationMembers[y].name)) {
+          continue;
+        } else {
+          expect(currentGenerationMembers[y].parents).toHaveLength(2);
+          expect(currentGenerationMembers[y].parents[0]).toBeInstanceOf(
+            royalFamilyModule.Person
+          );
+          expect(currentGenerationMembers[y].parents[1]).toBeInstanceOf(
+            royalFamilyModule.Person
+          );
+        }
+      }
+    }
+  });
 });
