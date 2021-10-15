c9406ce0e7d961fcfc68db743639207a9a388a3a
Myles Gordon <myles.gordon@sky.uk>
2021-10-15 15:07:49 +0100

    Updated documentation

--- assignments/airport/Person.js
+++ assignments/airport/Person.js
@@ -1,12 +1,23 @@
 const { Check } = require("./Check");
 
+/**
+ * Represents any person within the airport system, such as a passenger or crew member
+ * @class
+ */
 class Person {
+  /**
+   * The only requirement for a person is that they have a name
+   * @param {string} name
+   */
   constructor(name) {
     this.name = name;
     this.bags = [];
 
     this.isValidPerson();
   }
+  /**
+   * Checks whether the person has a valid name
+   */
   isValidPerson() {
     if (!Check.isStringValid(this.name)) {
       throw "Person requires a valid name";
