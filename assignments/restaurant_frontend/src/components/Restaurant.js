6768680b5b6d98e012246f58fdb13b4fcd23d9de
Myles Gordon <myles.gordon@sky.uk>
2021-11-05 11:00:47 +0000

    frontend: delete working

--- assignments/restaurant_frontend/src/components/Restaurant.js
+++ assignments/restaurant_frontend/src/components/Restaurant.js
@@ -1,10 +1,13 @@
-const Restaurant = ({ restaurant, updateMenu }) => {
+const Restaurant = ({ restaurant, updateMenu, deleteRestaurant }) => {
   return (
     <div>
+      <h3>{restaurant.name}</h3>
+      <img src={restaurant.imageLink} alt="Restaurant" className="preview" />
+      <br />
       <a href="#menu" onClick={() => updateMenu(restaurant.id)}>
-        <h3>{restaurant.name}</h3>
-        <img src={restaurant.imageLink} alt="Restaurant" className="preview" />
+        <button>Menu</button>
       </a>
+      <button onClick={() => deleteRestaurant(restaurant.id)}>Delete</button>
     </div>
   );
 };
--- assignments/restaurant_frontend/src/components/Restaurants.js
+++ assignments/restaurant_frontend/src/components/Restaurants.js
@@ -4,13 +4,29 @@ import Restaurant from "./Restaurant";
 const Restauraunts = ({ updateMenu }) => {
   const [restaurantState, setRestaurauntState] = useState([]);
 
-  useEffect(() => {
+  const fetchRestaurants = () => {
     fetch("http://localhost:3002/api/restaurant", {
       method: "GET",
     })
       .then((response) => response.json())
       .then((response) => setRestaurauntState(response))
       .catch((error) => console.error(error));
+  };
+
+  const deleteRestaurant = (id) => {
+    fetch("http://localhost:3002/api/restaurant", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: `{"id": ${id}}`,
+    })
+      .then(() => fetchRestaurants())
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
   }, []);
 
   return (
@@ -19,6 +35,7 @@ const Restauraunts = ({ updateMenu }) => {
         <Restaurant
           restaurant={restaurant}
           updateMenu={updateMenu}
+          deleteRestaurant={deleteRestaurant}
           key={restaurant.id}
         />
       ))}
